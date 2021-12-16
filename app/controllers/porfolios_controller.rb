9534a0f77300e0dd97df17df3546ba6a4111cd70
ZNH <zinnwe0720@gmail.com>
2021-12-17 05:25:44 +0900

    Implemented edit and update functionality

--- app/controllers/porfolios_controller.rb
+++ app/controllers/porfolios_controller.rb
@@ -20,4 +20,19 @@ class PorfoliosController < ApplicationController
 	      end
     end
   end
+
+  def edit
+  	@portfolio_item = Porfolio.find(params[:id])
+  end
+
+  def update
+  	@portfolio_item = Porfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:porfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to porfolios_path, notice: "Blog was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
 end
